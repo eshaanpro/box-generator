b6ba8c9223ce2a9915e0195a678e581b0b668594
eshaanpro <70801092+eshaanpro@users.noreply.github.com>
2020-11-11 17:46:40 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var button=document.getElementById("button");
+var numberInShape=1;
+var oldValue=0;
+
+button.onclick=()=>{
+    const circle=document.getElementById("circle");
+    const square=document.getElementById("square");
+    const rectangle=document.getElementById("rectangle");
+    const $box=document.getElementById("box");
+    let number=document.getElementById("number").value;
+    number=Number(oldValue)+Number(number);
+    let j;
+    for(let j=numberInShape;j<=number;j++){
+        var shape=document.createElement("div");
+        shape.innerHTML+=numberInShape;
+        if(square.checked){
+            shape.classList.add("square");
+        }
+        else if(circle.checked){
+            shape.classList.add("circle");
+        }
+        else if(rectangle.checked){
+            shape.classList.add("rectangle");
+        }
+        $box.appendChild(shape);
+        numberInShape++;
+        oldValue=document.getElementById("box").lastElementChild.innerHTML;
+    }
+}
